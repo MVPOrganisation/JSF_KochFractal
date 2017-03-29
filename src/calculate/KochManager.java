ecce4843a12258d9bd55ade71420f3cf8ef4dd0d
Meijer <max1_meijer@hotmail.com>
2017-03-29 11:28:25 +0200

    Code cleanup

--- src/calculate/KochManager.java
+++ src/calculate/KochManager.java
@@ -1,9 +1,6 @@
 package calculate;
 
-import javafx.application.Platform;
-import javafx.concurrent.Task;
 import jsf31kochfractalfx.JSF31KochFractalFX;
-import sun.nio.ch.ThreadPool;
 import timeutil.TimeStamp;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -15,27 +12,21 @@ import java.util.concurrent.Future;
  * @author Max Meijer
  * Creation date 15/03/2017.
  */
-public class KochManager implements Observer {
+public class KochManager {
 
     private KochFractal kf;
     private JSF31KochFractalFX application;
     private List<Edge> edges;
-    private int counter;
     private ExecutorService pool;
-
-
 
     public KochManager(JSF31KochFractalFX application) {
         kf = new KochFractal();
         this.application = application;
         edges =  Collections.synchronizedList(new ArrayList());
-        counter = 0;
-
     }
 
     public void changeLevel(int nxt) throws ExecutionException, InterruptedException {
         edges.clear();
-
         pool = Executors.newFixedThreadPool(3);
 
         EdgeCalculator edgeBot = new EdgeCalculator(0);
@@ -47,8 +38,6 @@ public class KochManager implements Observer {
         edgeRight.setLevel(nxt);
 
         TimeStamp ts = new TimeStamp();
-
-
         ts.setBegin("Starting calculation");
 
         Future<ArrayList<Edge>> futLeft = pool.submit(edgeLeft);
@@ -62,8 +51,6 @@ public class KochManager implements Observer {
         ts.setEnd("Finished calculation");
         requestDrawing();
         application.setTextCalc(ts.toString());
-        counter = 0;
-
     }
 
     public void drawEdges() {
@@ -81,27 +68,7 @@ public class KochManager implements Observer {
         application.setTextDraw(tsd.toString());
     }
 
-    public void requestDrawing() {
+    private void requestDrawing() {
         application.requestDrawEdges();
-    }
-
-    // Add a single edge to the local collection
-    synchronized void addEdge(Edge edge) {
-        edges.add(edge);
-    }
-
-    // Increase the counter to show what edge generation has finished
-    synchronized  void increaseCounter() {
-        counter++;
-    }
-
-    // Add all the calculated edges at once
-    synchronized void addEdges(ArrayList<Edge> newEdges) {
-        edges.addAll(newEdges);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-        edges.add((Edge) arg);
     }
 }
